26368c1a6fb255e428fce216ebdd02da859d173f
Ryan Apking <ryan.apking@gmail.com>
2016-09-16 08:27:17 -0700

    add PHPUnit test for first Specification: two matching strings.

---
+++ src/RepeatCounter.php
@@ -0,0 +1,9 @@
+<?php
+    class RepeatCounter
+    {
+        function countRepeats($searchInput, $findInput)
+        {
+
+        }
+    }
+ ?>
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -3,6 +3,16 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-        
+        function test_countRepeats_matchingStrings()
+        {
+            // Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $searchInput = "simple";
+            $findInput = "simple";
+            // Act
+            $result = $test_RepeatCounter->countRepeats($searchInput, $findInput);
+            // Assert
+            $this->assertEquals(1, $result);
+        }
     }
  ?>
