48566702d9dd4a8c548359ad3f6dfeb62f90ceef
Ryan Apking <ryan.apking@gmail.com>
2016-09-16 09:11:08 -0700

    correct countRepeats method in RepeatCounter to allow for quotation marks.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -6,8 +6,11 @@
             $repeatCounter = 0;
             $searchArray = explode(" ", $searchInput);
             foreach ($searchArray as $word) {
+                // Remove quotation marks before searching
+                $findInput = str_replace("\"", "", $findInput);
+                // Test for match, then remove punctuation that might interfere with a match and test again
                 if ($findInput == $word ||
-                $findInput == preg_replace('([?!.,\'"])', "", $word)) {
+                $findInput == preg_replace('([?!.,\'])', "", $word)) {
                     $repeatCounter++;
                 }
             }
